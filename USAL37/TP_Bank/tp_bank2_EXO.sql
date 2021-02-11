c0d5a32cc0260f39ce43a1c2943cc4b078408aae
MDevoldere <mdevoldere@users.noreply.github.com>
2021-02-11 12:55:14 +0100

    update

--- USAL37/TP_Bank/tp_bank2_EXO.sql
+++ USAL37/TP_Bank/tp_bank2_EXO.sql
@@ -1,13 +1,21 @@
-SELECT account_id, account_owner, account_balance, bank_name 
-FROM accounts 
-INNER JOIN banks ON banks.bank_id = accounts.bank_id; 
+USE usal37_tp_bank;
 
-
-SELECT account_id, account_owner, account_balance, bank_name 
-FROM accounts 
-LEFT JOIN banks ON banks.bank_id = accounts.bank_id; 
-
+SELECT * FROM accounts;
 
 SELECT account_id, account_owner, account_balance, bank_name 
 FROM accounts 
-RIGHT JOIN banks ON banks.bank_id = accounts.bank_id; 
+INNER JOIN banks ON banks.bank_id = accounts.bank_id
+WHERE account_id > 20;
+
+
+SELECT account_id, account_owner, account_balance, bank_name 
+FROM accounts 
+LEFT JOIN banks ON banks.bank_id = accounts.bank_id
+WHERE account_id > 20; 
+
+
+
+
+
+
+
--- USAL37/TP_Bank/tp_bank2_select.sql
+++ USAL37/TP_Bank/tp_bank2_select.sql
@@ -21,3 +21,17 @@ FROM accounts,banks WHERE banks.bank_id = accounts.bank_id;
 SELECT account_id, account_owner, account_balance, bank_id 
 FROM accounts;
 
+
+
+SELECT * FROM accounts 
+WHERE account_id > 10
+ORDER BY bank_id DESC, account_balance ASC, account_owner ASC
+;
+
+SELECT * FROM accounts 
+WHERE account_id > 10 
+ORDER BY account_balance DESC 
+LIMIT 2,10;
+
+UPDATE accounts SET account_balance=3900 WHERE account_id=11 LIMIT 1;
+
